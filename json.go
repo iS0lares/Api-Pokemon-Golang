b3057fb57b042893e001aeb696aba6125c7391d2
iS0lares <moisessolaressantos@gmail.com>
2024-10-15 21:35:04 -0300

    Create new function to request image of pokemon

--- json.go
+++ json.go
@@ -3,20 +3,32 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 )
-
-type Config struct {
-    Addr string `json:"addr"`
-    Protocol string `json:"protocol"`
-    Type string  `json:"type"`
-}
 
 type Pokemon struct {
     ID string `json:"id"`
     Name string `json:"name"`
     Image string `json:"img"`
     Type string `json:"type"`
+}
+
+func getPokemonImage(urlPokemon string) {
+    response, err := http.Get(urlPokemon)
+    if err != nil {
+        panic(err)
+    }
+    defer response.Body.Close()
+
+    body, err := io.ReadAll(response.Body)
+    if err != nil {
+        fmt.Println("Deu ruim pra ler a resposta da requisição")
+        return
+    }
+
+    fmt.Println(string(body))
 }
 
 func main() {
@@ -29,7 +41,8 @@ func main() {
     json.Unmarshal(data, &pokemons)
     
     for _, p := range pokemons {
-        fmt.Println(p.Name)
+        // fmt.Println(p.Name)
+        getPokemonImage(p.Image)
     }
 
 
